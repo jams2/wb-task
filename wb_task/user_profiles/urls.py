b55d78de5e3cfe4ddb43917052923128e8991afc
jams2 <joshamunn@gmail.com>
2020-05-09 10:41:23 +0100

    updates url schema

--- wb_task/user_profiles/urls.py
+++ wb_task/user_profiles/urls.py
@@ -6,6 +6,6 @@ app_name = "user_profiles"
 urlpatterns = [
     path("", IndexView.as_view(), name="user-profile-index"),
     path("create/", UserProfileCreate.as_view(), name="user-profile-create"),
-    path("update/<int:pk>/", UserProfileUpdate.as_view(), name="user-profile-update",),
-    path("delete/<int:pk>/", UserProfileDelete.as_view(), name="user-profile-delete",),
+    path("<int:pk>/update/", UserProfileUpdate.as_view(), name="user-profile-update",),
+    path("<int:pk>/delete/", UserProfileDelete.as_view(), name="user-profile-delete",),
 ]
